5137b8dcada4add5c84a2d0230392f8a0509305b
Jose David <jose7312000@gmail.com>
2022-09-22 12:37:55 -0500

    Agregando servidor para conectarse desde cualquier dispositivo conectado desde la red

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');//Exportamos express
+/*const express = require('express');//Exportamos express
 const app = express();//Creamos una aplicacion de express
 const port = 3000; //Definimos el puerto donde corre la aplicacion
 
@@ -15,4 +15,14 @@ app.get('/productos', (req, res) =>{
 
 app.listen(port, ()=>{ //Definimos con listem, el pueto de nuestra aplicacion para que lo escuche
     console.log("Corriendo sin problemas");
-})
+})*/
+
+const express = require('express');
+const app = express();
+const port = 8080;
+//aquí va tu ip
+const IP = " 192.168.1.5";
+
+app.listen(port, () => {
+    console.log("http://"+ IP +":" + port + "/");
+  });
