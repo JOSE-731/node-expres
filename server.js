c2fba0b07d36a75ea9908e28b70520667397605a
Jose David <jose7312000@gmail.com>
2022-09-22 12:15:15 -0500

    Creando ruta y servidor mas optimizado

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');//Exportamos express
+const app = express();//Creamos una aplicacion de express
+const port = 3000; //Definimos el puerto donde corre la aplicacion
+
+app.get('/', (req, res) => { //Ruta
+    res.send('Hola mundo');
+})
+
+app.listen(port, ()=>{ //Definimos con listem, el pueto de nuestra aplicacion para que lo escuche
+    console.log("Corriendo sin problemas");
+})
