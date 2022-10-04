097d04c16dab8f5a2aec66e8c847e99fdd642083
Jose David <jose7312000@gmail.com>
2022-10-04 16:37:00 -0500

    Crud

--- router/user.router.js
+++ router/user.router.js
@@ -1,5 +1,5 @@
 const express = require('express');//Exportamos express
-const UserServices = require('../services/user.services');
+const UserServices = require('./../services/user.services');
 
 const router = express.Router();
 const service = new UserServices;
@@ -12,5 +12,38 @@ router.get('/',  async (req, res) => {
         console.log(error)
     }
 })
+
+//Ruta post
+router.post('/', async (req, res) => {
+    try {
+        const newUser = req.body; //Obtenemos todo la respuesta de lo que se envia por post
+        const data = await service.create(newUser);
+        res.status(201).json({
+            message: 'created', //Le imprimimos un mensaje
+            data: newUser//retornamos la data
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: 'error', //Le imprimimos un mensaje
+        })
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log(id)
+        const data = await service.delete(id);
+        res.status(201).json({
+            message: 'delete', //Le imprimimos un mensaje
+            data: data//retornamos la data
+        })
+    } catch (error) {
+        res.status(404).json({
+            message: 'error', //Le imprimimos un mensaje
+        })
+    }
+})
+
 
 module.exports = router; //Importamos el modulo router
--- services/user.services.js
+++ services/user.services.js
@@ -1,17 +1,35 @@
 const getConnection = require('../libs/postgress');
 const pool = require('../libs/postgress');
-const {models} = require('../libs/sequelize'); //Exportamos los modelos
+const { models } = require('../libs/sequelize'); //Exportamos los modelos
 
 class UserServices {
 
-    constructor(){
+    constructor() {
         this.pool = pool;
         this.pool.on('error', (err) => console.log(error));//Escuchar si hay algun error en la conexion
+    }
+
+    async create(data) {
+        console.log(data)
+        const newUser = await models.User.create(data);
+        return newUser;
     }
 
     async find() {
         const rta = await models.User.findAll();//Optenemos todos los registros
         return rta;
+    }
+
+    async update(id, changes) {
+        const user = await models.User.findByPk(id); //Buscamos el usuario
+        const rta = await user.update(changes);//Hacemos update
+        return rta;
+    }
+
+    async delete(){
+        const user = await models.User.findByPk(id); //Buscamos el usuario
+        await user.destroy();//Eliminamos
+        return {id};
     }
 }
 
